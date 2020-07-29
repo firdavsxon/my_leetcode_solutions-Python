6a1f68133f28d467bdd3f7526e4b33d0a3712e9a
firdavsxon <akilovfs@gmail.com>
2020-07-29 14:23:18 -0700

    leetcode problems solutions

--- easy/007 Reverse Integer.py
+++ easy/007 Reverse Integer.py
@@ -3,15 +3,14 @@
 Given a 32-bit signed integer, reverse digits of an integer.
 
 Example 1:
-
 Input: 123
 Output: 321
-Example 2:
 
+Example 2:
 Input: -123
 Output: -321
-Example 3:
 
+Example 3:
 Input: 120
 Output: 21
 Note:
