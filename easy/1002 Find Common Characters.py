007351d396a6224630518d3af8530ddbb9385544
firdavsxon <akilovfs@gmail.com>
2020-07-31 18:06:17 -0700

    easy level leetcode solution

---
+++ easy/1002 Find Common Characters.py
@@ -0,0 +1,51 @@
+"""
+Given an array A of strings made only from
+lowercase letters, return a list of all
+characters that show up in all strings within
+the list (including duplicates).
+For example, if a character occurs 3 times in all
+strings but not 4 times, you need to include that
+character three times in the final answer.
+
+You may return the answer in any order.
+
+
+
+Example 1:
+
+Input: ["bella","label","roller"]
+Output: ["e","l","l"]
+Example 2:
+
+Input: ["cool","lock","cook"]
+Output: ["c","o"]
+
+
+Note:
+
+1 <= A.length <= 100
+1 <= A[i].length <= 100
+A[i][j] is a lowercase letter
+"""
+
+from typing import List
+from collections import Counter
+class Solution:
+	def commonChars(self, A: List[str]) -> List[str]:
+		pass
+
+	def commonChars1(self, A: List[str]):
+		output = Counter(A[0])
+
+		for word in A:
+			output &= Counter(word)
+
+		return [i for i in output.elements()]
+
+
+
+
+
+test = Solution()
+print(test.commonChars1(["bella","label","roller"]))
+print(test.commonChars1(["cool","lock","cook"]))
