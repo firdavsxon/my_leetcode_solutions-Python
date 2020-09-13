51d91a9df6fd33a661a61bd96656b36860d0c8e7
firdavsxon <akilovfs@gmail.com>
2020-09-13 11:20:45 -0700

    leetcode ahrd level problem solution

--- hard/30 Substring with Concatenation of All Words.py
+++ hard/30 Substring with Concatenation of All Words.py
@@ -1,40 +1,75 @@
 """
-Given a list of words (without duplicates), please write a program that returns all concatenated words in the given list of words.
-A concatenated word is defined as a string that is comprised entirely of at least two shorter words in the given array.
+You are given a string s and an array of strings words of the same length. Return all starting indices of substring(s) in s that is a concatenation of each word in words exactly once, in any order, and without any intervening characters.
 
-Example:
+You can return the answer in any order.
 
-Input: ["cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"]
 
-Output: ["catsdogcats","dogcatsdog","ratcatdogcat"]
 
-Explanation: "catsdogcats" can be concatenated by "cats", "dog" and "cats";
- "dogcatsdog" can be concatenated by "dog", "cats" and "dog";
-"ratcatdogcat" can be concatenated by "rat", "cat", "dog" and "cat".
-Note:
+Example 1:
 
-The number of elements of the given array will not exceed 10,000
-The length sum of elements in the given array will not exceed 600,000.
-All the input string will only include lower case letters.
-The returned elements order does not matter.
+Input: s = "barfoothefoobarman", words = ["foo","bar"]
+Output: [0,9]
+Explanation: Substrings starting at index 0 and 9 are "barfoo" and "foobar" respectively.
+The output order does not matter, returning [9,0] is fine too.
+Example 2:
 
+Input: s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"]
+Output: []
+Example 3:
+
+Input: s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]
+Output: [6,9,12]
+
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of lower-case English letters.
+1 <= words.length <= 5000
+1 <= words[i].length <= 30
+words[i] consists of lower-case English letters.
 """
 from typing import List
 
 
 class Solution:
-	def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
-		if len(words) == 0 or len(words)[0] == 0:
+	def findAllConcatenatedWordsInADict(self, s: str, words: List[str]) -> List[int]:
+		if len(words) == 0 or len(words[0]) == 0:
 			return []
 		words_frequency = {}
 		for i in words:
 			words_frequency[i] = words_frequency.get(i,0) + 1
 
-		result_indeces = []
+		result_indices = []
 		words_count = len(words)
-		word_length = len(words)[0]
+		word_length = len(words[0])
 
-		for i in range(len(words)):
+		for i in range((len(s) - words_count*word_length)+1):
+			words_seen = {}
+			for j in range(0, words_count):
+				next_word_index = i + j*word_length
+				# get the next word from the string
+				word = s[next_word_index:next_word_index+word_length]
+				if word not in words_frequency: # break, we don't need this word
+					break
+
+				# Add the word to the 'word_seen' map
+				if word not in words_seen:
+					words_seen[word] = 0
+				words_seen[word] += 1
+
+				# No need to process further if the word has higher frequency required
+				if words_seen[word]> words_frequency.get(word,0):
+					break
+
+				if j+1 == words_count: # Store index if we have found all words
+					result_indices.append(i)
+
+		return result_indices
+
+test = Solution()
+print(test.findAllConcatenatedWordsInADict("catfoxcat", ["cat", "fox"]))
+print(test.findAllConcatenatedWordsInADict("wordgoodgoodgoodbestword", ["word","good","best","word"]))
 
 
 
