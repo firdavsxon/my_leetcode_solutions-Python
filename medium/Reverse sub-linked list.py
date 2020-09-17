754b941bc85f55c3f2cd90dad7b971d8039a1f95
firdavsxon <akilovfs@gmail.com>
2020-09-17 09:29:42 -0700

    reversing sublist in singly linked list

--- medium/Reverse sub-linked list.py
+++ medium/Reverse sub-linked list.py
@@ -18,8 +18,42 @@ class Node:
 			temp = temp.next
 		print()
 
+
 def reverse_sub(head, p, q):
-	pass
+	if p ==q:
+		return head
+	# after skipping 'p-1' nodes, current will point to 'p'th node
+	i, prev, current = 0, None, head
+	while current and i < p-1:
+		prev = current
+		current = current.next
+		i+=1
+	# we are interested in three part of the linked list, the part before index 'p',
+	# the part between 'p' and 'q', and the part after index 'q'
+	last_node_of_first_part = prev
+	# after reversing the linked list 'current' will become the last node of the sub-list
+	last_node_of_sub_list = current
+
+	i=0
+	# reverse nodes between 'p' and 'q'
+	while current and i < q-p+1:
+		temp_next = current.next # temp_next will temporarily have next node
+		current.next = prev
+		prev = current
+		current = temp_next
+		i += 1
+
+	# connect with the first part
+	if last_node_of_first_part:
+		# 'prev' is now the first node of the sub-list
+		last_node_of_first_part.next = prev
+		# this means p = 1 i.e., we are changing the first node (head) of the linked list
+	else:
+		head = prev
+	# connect with the last part
+	last_node_of_sub_list.next = current
+	return head
+
 
 def main():
 	head = Node(1)
@@ -32,8 +66,8 @@ def main():
 	head.print_list()
 	result = reverse_sub(head, 2, 4)
 
-	print("Nodes after linnked list sub list reversed: ", end= '-> ')
-	# result.print_list()
+	print("Nodes after linnked list sub list reversed: ", end='-> ')
+	result.print_list()
 
 main()
 
