73cc6b7bba310ddd25ec9d9943668376893cf1c9
firdavsxon <akilovfs@gmail.com>
2020-09-22 19:44:30 -0700

    easy level problem solution

--- easy/google_test.py
+++ easy/google_test.py
@@ -2,10 +2,15 @@
 from typing import List
 
 def utf8char_len(lst: List[bytes]):
-	return len(lst)
+	length = 0
+	for num in lst:
+		bin_repr = format(num, '#010b')[-8:]
+		t = type(bin_repr)
+		for bit in bin_repr:
+			if bit== '0': break
+			length += 1
+	return length
 
 
-print(utf8char_len(bytearray(11100000)))
+print(utf8char_len(0b10011001))
 
-import locale
-print(locale.getpreferredencoding())
