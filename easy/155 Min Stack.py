75798f0761da333f18b6ca3f0a9b7cb4240d7944
firdavsxon <akilovfs@gmail.com>
2020-11-04 16:47:34 -0800

    adding sceleton of the project

--- easy/155 Min Stack.py
+++ easy/155 Min Stack.py
@@ -27,3 +27,30 @@ minStack.top();    // return 0
 minStack.getMin(); // return -2
 
 """
+
+
+class MinStack:
+
+	def __init__(self):
+		"""
+		initialize your data structure here.
+		"""
+
+	def push(self, x: int) -> None:
+		pass
+
+	def pop(self) -> None:
+		pass
+
+	def top(self) -> int:
+		pass
+
+	def getMin(self) -> int:
+		pass
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
