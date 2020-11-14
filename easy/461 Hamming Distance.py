34030ebebbe7504255a73fbfda2ca9fbb8ed3ed0
firdavsxon <akilovfs@gmail.com>
2020-11-13 17:21:58 -0800

    easy level problem leetcode

--- easy/461 Hamming Distance.py
+++ easy/461 Hamming Distance.py
@@ -1,5 +1,6 @@
 """
-The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+The Hamming distance between two integers is the number of positions
+at which the corresponding bits are different.
 
 Given two integers x and y, calculate the Hamming distance.
 
@@ -10,7 +11,7 @@ Example:
 
 Input: x = 1, y = 4
 
-Output: 2
+Output:
 
 Explanation:
 1   (0 0 0 1)
