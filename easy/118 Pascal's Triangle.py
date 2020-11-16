2b1f6c4c2bb8b94626937e12f0388879bcda4d4b
firdavsxon <akilovfs@gmail.com>
2020-11-16 10:11:55 -0800

    easy level problem solution on leetcode

--- easy/118 Pascal's Triangle.py
+++ easy/118 Pascal's Triangle.py
@@ -16,3 +16,19 @@ Output:
  [1,4,6,4,1]
 ]
 """
+from typing import List
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        out = []
+        for i in range(numRows):
+            temp = [None for _ in range(i+1)]
+            temp[0], temp[-1] = 1, 1
+            j=1
+            while j<len(temp)-1:
+                temp[j] = out[i-1][j-1] + out[i-1][j]
+                j+=1
+            out.append(temp)
+        return out
+
+test = Solution()
+print(test.generate(5))
