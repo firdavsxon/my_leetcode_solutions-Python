dadff32b73bee83956653d5c56e72547cf510ee1
firdavsxon <akilovfs@gmail.com>
2020-12-03 16:43:42 -0800

    medium level leetcode problem solution

--- medium/5 Longest Palindromic Substring.py
+++ medium/5 Longest Palindromic Substring.py
@@ -28,13 +28,30 @@ Constraints:
 s consist of only digits and English letters (lower-case and/or upper-case),
 
 
-
+#  xerexs
 """
 
 
 class Solution:
 	def longestPalindrome(self, s: str) -> str:
-		pass
-	
+		def expand_around_center(s: str, left: int, right: int):
+			L, R = left, right
+			while L >= 0 and R<len(s) and s[L] == s[R]:
+				L-=1
+				R+=1
+			return R-L-1
+
+		start, end = 0, 0
+		for window_end, ch in enumerate(s):
+			l1 = expand_around_center(s, window_end, window_end)
+			l2 = expand_around_center(s, window_end, window_end+1)
+			l = max(l1,l2)
+			if l > end-start:
+				start = window_end - (l-1)//2
+				end = window_end+l//2
+		return s[start: end+1]
+
 test = Solution()
-print(test.longestPalindrome("babad"))
+s1= "lipwawibllrziekxgwudqghfpvsafguorthpsdihcinuasyzmttzxdluhrnfdrawabwxdgpoqabfhutzowqfhkynrhobyuygesngyxpjyilqhwyeemklicinmatyishobtitukbkpqtxwioqnztlewilnewokfqkycfuvgqmogwuvkrxphyjvhbkhpcwywfnazsoulmgdoaxyngoynmfexdcpanoyidutpzcicibjnzmybvggqbpbejsvliocotewgrfcwyebisiywjsugjxxwupryxglvkgdugbejsibuscjofrvaeexqweieldfhriftlczbuzmuizjqzxovziflaigwxrxowmhdlvrbxzeaaqxmicvigolodopbukjvkzwvxexnnweodsoscnpmuwgjhmlurwdqbwrzavjjubsueahunqwemmewqnuhaeusbujjvazrwbqdwrulmhjgwumpncsosdoewnnxexvwzkvjkubpodologivcimxqaaezxbrvldhmwoxrxwgialfizvoxzqjziumzubzcltfirhfdleiewqxeeavrfojcsubisjebgudgkvlgxyrpuwxxjgusjwyisibeywcfrgwetocoilvsjebpbqggvbymznjbiciczptudiyonapcdxefmnyognyxaodgmluoszanfwywcphkbhvjyhpxrkvuwgomqgvufcykqfkowenliweltznqoiwxtqpkbkutitbohsiytamnicilkmeeywhqliyjpxygnsegyuybohrnykhfqwoztuhfbaqopgdxwbawardfnrhuldxzttmzysaunichidsphtrougfasvpfhgqduwgxkeizrllbiwawpil"
+s2= "babad"
+print(test.longestPalindrome(s2))
