0eb26e81be7560f9f5cd7cd01cbe53a733eba2a1
firdavsxon <akilovfs@gmail.com>
2021-05-09 21:50:41 -0700

    medium

--- medium/1214 Two sum BSTs.py
+++ medium/1214 Two sum BSTs.py
@@ -28,6 +28,38 @@ class TreeNode:
 
 class Solution:
 	def twoSumBSTs(self, root1: TreeNode, root2: TreeNode, target: int) -> bool:
-		pass
+		r1 = []
+		r2 = []
+
+		def traverse(root, lst):
+			if not root:
+				return lst
+			lst.append(root.val)
+			if root.left:
+				traverse(root.left, lst)
+			if root.right:
+				traverse(root.right, lst)
+			return lst
+
+		ro1 = traverse(root1, r1)
+		ro2 = traverse(root2, r2)
+		for i in ro1:
+			if target - i in ro2:
+				return True
+		return False
 
 
+root1 = TreeNode(2)
+root1.left = TreeNode(1)
+root1.right = TreeNode(4)
+
+root2 = TreeNode(1)
+root2.left = TreeNode(0)
+root2.right = TreeNode(3)
+
+
+
+
+
+test = Solution()
+print(test.twoSumBSTs(root1, root2, target=5))
