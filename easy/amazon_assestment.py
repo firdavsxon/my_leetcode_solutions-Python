0ac5effc332324c0d2ae22dd962cfbb1051c88f7
firdavsxon <akilovfs@gmail.com>
2021-08-09 14:30:12 -0700

    update

---
+++ easy/amazon_assestment.py
@@ -0,0 +1,22 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows == len(s):
+            return s
+        arr = ['']*numRows
+        cycle = 0
+        j=0
+        for i in range(len(s)):
+            arr[j] = arr[j]+ s[i]
+            if not cycle:
+                j = j+1
+            else:
+                j = j-1
+            if j+1 == numRows:
+                cycle = 1
+            if j == 0:
+                cycle = 0
+        return ''.join(arr)
+
+test = Solution()
+s = "PAYPALISHIRING"
+print(test.convert(s, numRows=3))
