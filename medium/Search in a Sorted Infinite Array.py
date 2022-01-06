c5963184c7af0c825a3e271b00ade9fd52c8e0b7
firdavsxon <akilovfs@gmail.com>
2022-01-06 12:39:37 -0800

    inifinte array index finding

--- medium/Search in a Sorted Infinite Array.py
+++ medium/Search in a Sorted Infinite Array.py
@@ -27,20 +27,36 @@ Explanation: The key is not present in the array.
 
 """
 class ArrayReaders:
-	def __init_(self, arr):
+	def __init__(self, arr):
 		self.arr = arr
 
-		def get(self, index):
-			if index>= len(self.arr):
-				return float('inf')
-			return self.arr[index]
+	def get(self, index):
+		if index>= len(self.arr):
+			return float('inf')
+		return self.arr[index]
 
 
 class Solution:
 	def search_in_infinite_array(self, reader, key):
+		start, end = 0, len(reader.arr)-1
+		if reader.get(end)==float('inf'):
+			return -1
+		while start <= end:
+			mid = start + (end-start)//2
 
-
+			if reader.arr[mid] == key:
+				return mid
+			elif reader.arr[mid] > key:
+				end = mid-1
+			else:
+				start = mid +1
 		return -1
 
+
+test = Solution()
+arr = [24,6,8,10,12,14,16,18,20,22,24,26,28,30]
+arr1 = [1,3,8,10,15]
+reader = ArrayReaders(arr1)
+print(test.search_in_infinite_array(reader, 200))
 
 test = Solution()
